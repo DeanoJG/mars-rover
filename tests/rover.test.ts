e1961d461afd0ad08b2f3d399e45bb8ea1beb516
Dean Lobo <dean.lobo@blackbaud.me>
2024-05-02 10:49:26 +0100

    mars and grid unit tests

--- tests/rover.test.ts
+++ tests/rover.test.ts
@@ -1,4 +1,4 @@
-import { Grid, Rover } from "../src/rover";
+import { Grid, Rover, mars } from "../src/rover";
 
 describe("Rover", () => {
     it("should create a new Rover", () => {
@@ -26,7 +26,7 @@ describe("Rover", () => {
         expect(rover.orientation).toBe("N");
     });
 
-    it("should move execute all instructions", () => {
+    it("should move execute all instructions given", () => {
         const grid = new Grid(4, 8)
         const rover = new Rover(2, 3, "N", "FLLFR");
         rover.executeInstructions(grid);
@@ -47,6 +47,80 @@ describe("Rover", () => {
         expect(rover.toString()).toBe("(0, 0, S) LOST");
     });
 
+    it("should store the last valid position if it gets lost", () => {
+        const rover = new Rover(0, 0, "N", "F");
+        rover.executeInstructions(new Grid(0, 0));
+        expect(rover.x).toBe(0);
+        expect(rover.y).toBe(0);
+        expect(rover.orientation).toBe("N");
+        expect(rover.isLost).toBe(true);
+    });
+
+    it("should not execute an instruction if the next position is occupied", () => {
+        const grid = new Grid(1, 1)
+        grid.addRover(new Rover(0, 1, "N", "F"));
+        grid.addRover(new Rover(0, 0, "N", "F"));
+        const secondRover = grid.rovers[1]
+        secondRover.executeInstructions(grid);
+        expect(secondRover.x).toBe(0);
+        expect(secondRover.y).toBe(0);
+    });
+
+});
+
+describe("Grid", () => {
+    it("should create a new Grid", () => {
+        const grid = new Grid(4, 8);
+        expect(grid).toBeDefined();
+        expect(grid.x).toBe(4);
+        expect(grid.y).toBe(8);
+        expect(grid.rovers).toHaveLength(0);
+    });
+
+    it("should add a rover to the grid", () => {
+        const grid = new Grid(4, 8);
+        const rover = new Rover(2, 3, "N", "FLLFR");
+        grid.addRover(rover);
+        expect(grid.rovers).toHaveLength(1);
+    });
+
+    it("should not add a rover to the grid if the position is occupied", () => {
+        const grid = new Grid(1, 1);
+        const rover = new Rover(0, 0, "N", "F");
+        grid.addRover(rover);
+        grid.addRover(rover);
+        expect(grid.rovers).toHaveLength(1);
+    });
+
+    it("should add rovers to the grid if the position is unoccupied", () => {
+        const grid = new Grid(1, 1);
+        const rover = new Rover(0, 0, "N", "F");
+        const rover2 = new Rover(0, 1, "N", "F");
+        grid.addRover(rover);
+        grid.addRover(rover2);
+        expect(grid.rovers).toHaveLength(2);
+    });
+
+
+
+});
+
+describe("mars", () => {
+    it("should correctly execute example 1", () => {
+        const rovers = [new Rover(2, 3, "E", "LFRFF"), new Rover(0, 2, "N", "FFLFRFF")];
+        const result = mars(4, 8, rovers);
+        expect(result.rovers).toHaveLength(2);
+        expect(result.rovers[0].toString()).toBe('(4, 4, E)');
+        expect(result.rovers[1].toString()).toBe('(0, 4, W) LOST');
+    });
+
+    it("should correctly execute example 2", () => {
+        const rovers = [new Rover(2, 3, "N", "FLLFR"), new Rover(1, 0, "S", "FFRLF")];
+        const result = mars(4, 8, rovers);
+        expect(result.rovers).toHaveLength(2);
+        expect(result.rovers[0].toString()).toBe('(2, 3, W)');
+        expect(result.rovers[1].toString()).toBe('(1, 0, S) LOST');
+    });
 
 
 });
